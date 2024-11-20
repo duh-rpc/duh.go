be5cda92e51581f425335bee0a88e57ca04b555a
Derrick J. Wippler <thrawn01@gmail.com>
2024-11-20 10:56:47 -0600

    WIP

--- retry/budget_test.go
+++ retry/budget_test.go
@@ -1,14 +1,1 @@
 package retry_test
-
-//func TestMovingRate(t *testing.T) {
-//	rate := retry.NewMovingRate(time.Minute, 60)
-//
-//	now := time.Now()
-//	rate.Add(5, now)
-//	rate.Add(3, now.Add(1*time.Second))
-//	rate.Add(20, now.Add(2*time.Second))
-//	rate.Add(20, now.Add(3*time.Second))
-//
-//	r := rate.Rate(now.Add(20 * time.Second))
-//	fmt.Printf("Current rate: %.2f hits per minute\n", r)
-//}
